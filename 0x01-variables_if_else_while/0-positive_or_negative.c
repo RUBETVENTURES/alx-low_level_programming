7c423fbef660a769ed5f3bb2486257fb8ce1df06
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-10 18:55:57 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
+/*
  * main - Print if the number is positve, zero or negative
  * 
  * Description: using the main function
  * this program prints "Programming is positive,  zero or negative
- * Return: Always 0
  */
 int main(void)
 {
