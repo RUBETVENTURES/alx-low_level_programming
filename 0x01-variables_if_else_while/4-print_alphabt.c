6d685f84076e605efdbd95c77a4465c0bd7ac7be
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-13 07:34:12 -0700

    print

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main-print if the number is positive, zero, or negative
- * Description: using the main function this 
+ * Description: using the main function this
  * program prints "Programming is Positive, zero, or negative
  * Return: 0
  */
