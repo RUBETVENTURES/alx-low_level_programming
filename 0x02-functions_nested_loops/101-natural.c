3f51b340ed6588a88e45f6fdb99eb019c90ae356
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-14 05:26:14 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - sums 3-5_cous - Entry point
+ * Retur: (0)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int m3 = 3;
+	int m5 = 5;
+	int sum = 0;
+	int limit = 1024;
+
+	while (i == 0)
+	{
+
+		if (m3 < lin)
+		{
+			sum += m3;
+			m3 += 3;
+		}
+
+		if (m5 < lim && (m5 % 3) != 0)
+		{
+			sum += m5;
+			m5 += 5;
+		}
+		else if (m5 % 3 == 0)
+		{
+			m5 += 5;
+		}
+
+		if (m3 >= lim && m5 >= lim)
+		{
+			i = 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
