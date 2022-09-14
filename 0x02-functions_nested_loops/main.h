23c911f294a1bf7bc116f7515e334ec44827a772
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-13 17:18:36 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-/**
- * main - check the code
- *
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(v0id);
+int _islower(int c);
+int _is alpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_laast_digit(int nld);
+void jack_bauer(void);
+void times_table(void);
+int add(int one, int two);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endif
