ea11c2948d9ec93488ce0fdfa456b66d6d27aad9
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-13 19:18:48 -0700

    main

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @x: an integer to add to y
+ * @y: an integer to add to x
+ * Return: void
+ */
+
+int add(int x; int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of an int
+ * @n: integer from where to get absolute value
+ * Return: Void
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
