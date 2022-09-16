267f84c00e241d52336d07176cdf15929574d229
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-16 15:48:31 -0700

    more numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 extern int _putchar(char c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
@@ -13,4 +14,6 @@ extern int print_square(int size);
 extern int fiss_buzz(void);
 extern int print_triangle(int size);
 extern int print_number(int n);
+
+
 #endif
