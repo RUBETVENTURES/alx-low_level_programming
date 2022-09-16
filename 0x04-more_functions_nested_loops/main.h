a1bf749a3b989825f10a19c744ad9df6be0fd885
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-16 15:52:55 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-extern int _putchar(char c);
-extern int _isupper(int c);
-extern int _isdigit(int c);
-extern int mul(int a, int b);
-extern int print_numbers(void);
-extern int print_most_numbers(void);
-extern int more_numbers(void);
-extern int print_line(int n);
-extern int print_diagonal(int n);
-extern int print_square(int size);
-extern int fiss_buzz(void);
-extern int print_triangle(int size);
-extern int print_number(int n);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int print_numbers(void);
+int print_most_numbers(void);
+int more_numbers(void);
+int print_line(int n);
+int print_diagonal(int n);
+int print_square(int size);
+int fiss_buzz(void);
+int print_triangle(int size);
+int print_number(int n);
 
 
 #endif
