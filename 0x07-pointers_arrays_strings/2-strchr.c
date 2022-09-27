9d6f72cac26d72f38bd35b7d1542b5b8028f7d8d
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-26 18:25:58 -0700

    chrstr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strch - points to the occurence
+ * @s: character
+ * @c: string
+ * Return: first occurrence of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	char x;
+	while (1)
+	{
+		x = *s++;
+		if (x == c)
+			return (s - 1);
+		if (!x)
+			return (0);
+	}
+}
