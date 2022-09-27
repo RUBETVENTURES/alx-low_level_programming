cc8c155d7d5eb0fcbfc567423ace99d538d6d9b2
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-27 03:02:43 -0700

    set

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,7 @@
  * @n: bytes to be used
  * Return: pointer to dest
  */
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
