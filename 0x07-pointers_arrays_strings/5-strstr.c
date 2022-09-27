d75c3a00cb34cde541bd552907918c635415efde
Ubong Akpan Etukudo <ubongaetukudo@yahoo.com>
2022-09-26 19:13:38 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strstr - ponter to substring
+ * @haystack: pointer a
+ * @needle: string to cut
+ * Return: pointer of choice
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	char *w, *x;
+
+	x = needle;
+
+	if (!*x)
+		return (haystack);
+	for (; haystack != 0; haystack++)
+	{
+		if (*haystack != *x)
+			continues;
+		w = haystack;
+		while (1)
+		{
+			if (!*x)
+				return (haystack);
+			if (*w++ != *x++)
+				break;
+		}
+		x = needle;
+	}
+	return (0);
+}
